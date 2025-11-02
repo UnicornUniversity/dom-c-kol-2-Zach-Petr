79c0a7d8ee19f974282746bf909f0f4148d4a80a
Zach Petr <zach.sluzby@gmail.com>
2025-11-02 14:05:19 +0100

    main.js

--- main.js
+++ main.js
@@ -1,59 +1,32 @@
-export function Převod(a, s){
-    let b = NaN; //proměnná pro ukládání zbytku a values mapy
-    let c = 0; // proměnná pro počet opakování a klíče mapy
-    let mapa = new Map(); // mapa pro ukládání výsledků
+ export function Převod(a, s){
+    if(a === 0) return "0";
+    let vysledek = "";
     while(a > 0){
-        b = a % s;
-        mapa.set(c , b);
-        a = Math.floor (a / s);
-        c++;
+        vysledek = (a % s) + vysledek;
+        a = Math.floor(a / s);
     }
-    let hodnoty = Array.from(mapa.values()).reverse();
-    let vysledek = hodnoty.join("");
     return vysledek;
 }
+
 export function PřevodNa16(a){
-    let b = NaN; //proměnná pro ukládání zbytku a values mapy
-    let c = 0; // proměnná pro počet opakování a klíče mapy
-    let mapa = new Map(); // mapa pro ukládání výsledků
+    if(a === 0) return "0";
+    const hex = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    let vysledek = "";
     while(a > 0){
-        b = a % 16;
-        if(b === 10){
-            b = "A";         // převádění zbytků na čísla
-        }
-        else if(b === 11){
-            b = "B";
-        }
-        else if(b === 12){
-            b = "C";
-        }
-        else if(b === 13){
-            b = "D";
-        }
-        else if(b === 14){
-            b = "E";
-        }
-        else if (b === 15){
-            b = "F";
-        }
-        mapa.set(c , b);
-        a = Math.floor (a / 16);
-        c++;
+        vysledek = hex[a % 16] + vysledek;
+        a = Math.floor(a / 16);
     }
-    let hodnoty = Array.from(mapa.values()).reverse();
-    let vysledek = hodnoty.join("");
     return vysledek;
 }
+
 let answer = prompt("Zadej číslo v desítkové soustavě:");
-
-
-
 const A = Number(answer);
+
 let vysledek1 = Převod(A, 2);
 let vysledek2 = Převod(A, 8);
 let vysledek3 = PřevodNa16(A);
+
 console.log(`Číslo: ${A}
 je v dvojkové soustavě: ${vysledek1},
-v osmičkové soustavě: ${vysledek2}
+v osmičkové soustavě: ${vysledek2},
 a v šestnáckové soustavě: ${vysledek3}.`);
-
