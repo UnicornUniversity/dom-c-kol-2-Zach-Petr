9d3a6b78b8d6523639725e7b44d1137a6f439381
Zach Petr <zach.sluzby@gmail.com>
2025-11-02 14:15:17 +0100

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
- export function Převod(a, s){
+/**
+ * Převádí číslo do dvjokové a osmičkové soustavy.
+ * @param {number} a je číslo v desítkové soustavě.
+ * @param {number} s dělitel a zároveň určuje sestavu.
+ * @returns {string} číslo v šestnáckové.
+ */
+export function Převod(a, s){
     if(a === 0) return "0";
     let vysledek = "";
     while(a > 0){
@@ -7,7 +13,11 @@
     }
     return vysledek;
 }
-
+/**
+ * Převádí číslo do šestnáctkové soustavy.
+ * @param {number} a je číslo v desítkové soustavě.
+ * @returns {string} číslo v šestnáckové.
+ */
 export function PřevodNa16(a){
     if(a === 0) return "0";
     const hex = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
